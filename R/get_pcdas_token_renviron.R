57027b83df961132faa2b751932f500017c72a12
Raphael Saldanha <rfsaldanha@gmail.com>
2022-07-25 15:27:12 -0300

    Document and export function get_pcdas_token_renviron.

--- R/get_pcdas_token_renviron.R
+++ R/get_pcdas_token_renviron.R
@@ -1,3 +1,11 @@
+#' Get PCDaS token from renviron
+#'
+#' @return A character.
+#'
+#' @details
+#' PCDaS token may be stored at renviron with `Sys.setenv("pcdas_token" = "YOUR_PCDAS_TOKEN")`. This function retrivies the token if stored at renviron.
+#'
+#' @export
 get_pcdas_token_renviron <- function(){
   pcdas_token <- Sys.getenv("pcdas_token")
   if(pcdas_token == ""){
