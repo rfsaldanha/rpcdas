6097e35f64e9bedb250284af72a86ddd7bf89705
Raphael Saldanha <rfsaldanha@gmail.com>
2022-07-25 15:37:07 -0300

    Updates error message.

--- R/get_pcdas_token_renviron.R
+++ R/get_pcdas_token_renviron.R
@@ -9,7 +9,7 @@
 get_pcdas_token_renviron <- function(){
   pcdas_token <- Sys.getenv("pcdas_token")
   if(pcdas_token == ""){
-    stop("PCDaS token API not provided and not found on renvirom. Please provide PCDaS API token.")
+    stop("PCDaS token API not provided and not found on renviron. Please provide PCDaS API token.")
   }
 
   return(pcdas_token)
