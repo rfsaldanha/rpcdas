82fbfbf141ddfb19c4a3b19f47d8fe4e057a13e9
Raphael Saldanha <rfsaldanha@gmail.com>
2023-04-24 17:26:47 +0200

    Enable multiple years and custom fetch_size.

--- R/get_sim.R
+++ R/get_sim.R
@@ -5,7 +5,8 @@
 #' This function uses raw data from the Sistema de Informações de Mortalidade (SIM) available at the PCDaS API. A documentation about this data can be found at \url{https://pcdas.icict.fiocruz.br/conjunto-de-dados/sistema-de-informacoes-de-mortalidade-sim/}.
 #'
 #' @param agg character. Spatial aggregation level. \code{uf_res} for UF of residence. \code{uf_ocor} for UF of occurrence. \code{regsaude_res} for regiao de saude of residence. \code{regsaude_ocor} for regiao de saúde of occurence. \code{mun_res} for municipality of residence. \code{mun_ocor} for municipality of ocurrence.
-#' @param ano numeric. Year of death.
+#' @param agg_time character. Time aggregation level. \code{year} for yearly data. \code{month} for monthly data. \code{week} for weekly data. Defaults to \code{year}.
+#' @param ano vector. Year of death.
 #' @param pcdas_token character. PCDaS API token. If not provided, the function will look for it on renvirom.
 #' @param sexo character. Sex of the deceased. \code{Masculino} for males, \code{Feminino} for females and \code{Ignorado} for unknown.
 #' @param idade_a numeric. Minimum age of the deceased, in years.
@@ -13,6 +14,7 @@
 #' @param cid_like character. CID-10 code of basic cause of death. Used with a \code{LIKE} operator.
 #' @param cid_in character vector. CID-10 codes of basic cause of death. Used with a \code{IN} operator.
 #' @param more_filters character. Additional filters can be added by using this parameter, with a SQL query.
+#' @param fetch_size character. Pagination size for API call.
 #'
 #' @details
 #' If \code{idade_a} is supplied, the query will filter records with age less or equal. If \code{idade_b} is supplied, the query will filter records with age more or equal. If both are supplied, the query will filter records in the interval, closed on both sides.
@@ -30,10 +32,11 @@
 #'
 #' @importFrom rlang .data
 #' @export
-get_sim <- function(agg, ano, pcdas_token = NULL, sexo = NULL, idade_a = NULL, idade_b = NULL, cid_like = NULL, cid_in = NULL, more_filters = NULL){
+get_sim <- function(agg, agg_time = "year", ano, pcdas_token = NULL, sexo = NULL, idade_a = NULL, idade_b = NULL, cid_like = NULL, cid_in = NULL, more_filters = NULL, fetch_size = 65000){
   # Function argument check
   checkmate::assert_choice(x = agg, choices = c("uf_res", "uf_ocor", "mun_res", "mun_ocor", "regsaude_res", "regsaude_ocor"))
-  checkmate::assert_number(x = ano, lower = 1996)
+  checkmate::assert_choice(x = agg_time, choices = c("year", "month", "week"))
+  checkmate::assert_vector(x = ano)
   checkmate::assert_string(x = pcdas_token, null.ok = TRUE)
   checkmate::assert_choice(x = sexo, choices = c("Masculino", "Feminino", "Ignorado"), null.ok = TRUE)
   checkmate::assert_number(x = idade_a, lower = 0, null.ok = TRUE)
@@ -70,8 +73,20 @@ get_sim <- function(agg, ano, pcdas_token = NULL, sexo = NULL, idade_a = NULL, i
   # SQL query basic partials
   sql_select <- glue::glue("SELECT {agg} AS agg, COUNT(1) AS freq")
   sql_from <- glue::glue("FROM \"datasus-sim\"")
-  sql_where <- glue::glue("WHERE ano_obito = {ano}")
+  sql_where <- glue::glue("WHERE ano_obito IN ({glue::glue_collapse(ano, sep = ', ')})")
   sql_group_by <- glue::glue("GROUP BY {agg}")
+
+  # Time aggregation additions
+  if(agg_time == "year"){
+    sql_select <- glue::glue(sql_select, ", DATETIME_FORMAT(data_obito, \'yyyy\') AS agg_time")
+    sql_group_by <- glue::glue(sql_group_by, ", agg_time")
+  } else if(agg_time == "month"){
+    sql_select <- glue::glue(sql_select, ", DATETIME_FORMAT(data_obito, \'yyyy-MM\') AS agg_time")
+    sql_group_by <- glue::glue(sql_group_by, ", agg_time")
+  } else if(agg_time == "week"){
+    sql_select <- glue::glue(sql_select, ", DATETIME_FORMAT(data_obito, \'yyyy-ww\') AS agg_time")
+    sql_group_by <- glue::glue(sql_group_by, ", agg_time")
+  }
 
   # Additions to where partial
   # Sexo
@@ -112,7 +127,7 @@ get_sim <- function(agg, ano, pcdas_token = NULL, sexo = NULL, idade_a = NULL, i
   sql_query <- glue::glue(sql_select, sql_from, sql_where, sql_group_by, .sep = " ")
 
   # Create list with token and SQL query
-  request_body <- list(token = list(token = pcdas_token), sql = list(sql = list(query = sql_query, fetch_size = 10000)))
+  request_body <- list(token = list(token = pcdas_token), sql = list(sql = list(query = sql_query, fetch_size = fetch_size)))
 
   # Request body as JSON
   request_body_json <- jsonlite::toJSON(request_body, auto_unbox = TRUE)
@@ -121,14 +136,14 @@ get_sim <- function(agg, ano, pcdas_token = NULL, sexo = NULL, idade_a = NULL, i
   content <- pcdas_query_request(body = request_body_json)
 
   # Transform content to data.frame and tibble
-  content_df <- convert_content_to_df(content) %>%
+  content_df <- convert_list_content_to_df(content) %>%
     tibble::as_tibble() %>%
     dplyr::mutate(
-      ano = ano,
       agg = as.numeric(.data$agg),
+      agg_time = .data$agg_time,
       freq = as.numeric(.data$freq)
     ) %>%
-    dplyr::select(.data$agg, .data$ano, .data$freq)
+    dplyr::select(.data$agg, .data$agg_time, .data$freq)
 
   return(content_df)
 }
