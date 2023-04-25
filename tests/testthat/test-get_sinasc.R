1039cb7a9f311acadfbf394e3194952c65979271
Raphael Saldanha <rfsaldanha@gmail.com>
2023-04-25 09:56:26 +0200

    Update SINASC tests to include multiple years and time aggregation of month and weeks.

--- tests/testthat/test-get_sinasc.R
+++ tests/testthat/test-get_sinasc.R
@@ -26,9 +26,30 @@ test_that("get sinasc with uf occurence works", {
   expect_gt(nrow(res), 20)
 })
 
-test_that("get sinasc with sex filterworks", {
+test_that("get sinasc with sex filter works", {
   res <- get_sinasc(agg = "mun_res", ano = 2010, sexo = "Masculino")
 
   expect_equal("tbl_df", class(res)[1])
   expect_gt(nrow(res), 4000)
+})
+
+test_that("get sinasc with uf residence and month aggregation works", {
+  res <- get_sinasc(agg = "uf_res", agg_time = "month", ano = 2010)
+
+  expect_equal("tbl_df", class(res)[1])
+  expect_gt(nrow(res), 300)
+})
+
+test_that("get sinasc with uf residence and week aggregation works", {
+  res <- get_sinasc(agg = "uf_res", agg_time = "week", ano = 2010)
+
+  expect_equal("tbl_df", class(res)[1])
+  expect_gt(nrow(res), 1400)
+})
+
+test_that("get sinasc with uf residence and multiple years works", {
+  res <- get_sinasc(agg = "uf_res", agg_time = "week", ano = c(2010, 2011))
+
+  expect_equal("tbl_df", class(res)[1])
+  expect_gt(nrow(res), 2800)
 })
