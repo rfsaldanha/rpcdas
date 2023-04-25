86bfaa9ad2e7c04adf7b3eb918eae509024f93a5
Raphael Saldanha <rfsaldanha@gmail.com>
2023-04-25 09:39:49 +0200

    Updates get_sih function with multiple years and month/week aggregations.

--- R/get_sih.R
+++ R/get_sih.R
@@ -5,6 +5,7 @@
 #' This function uses raw data from the Sistema de Informações de Hospitalares do SUS (SIH) available at the PCDaS API. A documentation about this data can be found at \url{https://pcdas.icict.fiocruz.br/conjunto-de-dados/sistema-de-informacoes-hospitalares-do-sus-sihsus/}.
 #'
 #' @param agg character. Spatial aggregation level. \code{uf_res} for UF of residence. \code{uf_ocor} for UF of occurrence. \code{regsaude_res} for regiao de saude of residence. \code{regsaude_ocor} for regiao de saúde of occurence. \code{mun_res} for municipality of residence. \code{mun_ocor} for municipality of ocurrence.
+#' @param agg_time character. Time aggregation level. \code{year} for yearly data. \code{month} for monthly data. \code{week} for weekly data. Defaults to \code{year}.
 #' @param ano numeric. Year of birth
 #' @param pcdas_token character. PCDaS API token. If not provided, the function will look for it on renvirom.
 #' @param sexo character. Sex of the new birth \code{Masculino} for males, \code{Feminino} for females and \code{Ignorado} for unknown.
@@ -22,10 +23,11 @@
 #'
 #' @importFrom rlang .data
 #' @export
-get_sih <- function(agg, ano, pcdas_token = NULL, sexo = NULL, idade_a = NULL, idade_b = NULL, more_filters = NULL){
+get_sih <- function(agg, agg_time = "year", ano, pcdas_token = NULL, sexo = NULL, idade_a = NULL, idade_b = NULL, more_filters = NULL, fetch_size = 65000){
   # Function argument check
   checkmate::assert_choice(x = agg, choices = c("uf_res", "uf_ocor", "mun_res", "mun_ocor", "regsaude_res", "regsaude_ocor"))
-  checkmate::assert_number(x = ano, lower = 1996)
+  checkmate::assert_choice(x = agg_time, choices = c("year", "month", "week"))
+  checkmate::assert_vector(x = ano)
   checkmate::assert_string(x = pcdas_token, null.ok = TRUE)
   checkmate::assert_choice(x = sexo, choices = c("Masculino", "Feminino", "Ignorado"), null.ok = TRUE)
   checkmate::assert_number(x = idade_a, lower = 0, null.ok = TRUE)
@@ -60,8 +62,20 @@ get_sih <- function(agg, ano, pcdas_token = NULL, sexo = NULL, idade_a = NULL, i
   # SQL query basic partials
   sql_select <- glue::glue("SELECT {agg} AS agg, COUNT(1) AS freq")
   sql_from <- glue::glue("FROM \"datasus-sih\"")
-  sql_where <- glue::glue("WHERE ano_internacao = {ano}")
+  sql_where <- glue::glue("WHERE ano_internacao IN ({glue::glue_collapse(ano, sep = ', ')})")
   sql_group_by <- glue::glue("GROUP BY {agg}")
+
+  # Time aggregation additions
+  if(agg_time == "year"){
+    sql_select <- glue::glue(sql_select, ", DATETIME_FORMAT(dt_inter, \'yyyy\') AS agg_time")
+    sql_group_by <- glue::glue(sql_group_by, ", agg_time")
+  } else if(agg_time == "month"){
+    sql_select <- glue::glue(sql_select, ", DATETIME_FORMAT(dt_inter, \'yyyy-MM\') AS agg_time")
+    sql_group_by <- glue::glue(sql_group_by, ", agg_time")
+  } else if(agg_time == "week"){
+    sql_select <- glue::glue(sql_select, ", DATETIME_FORMAT(dt_inter, \'yyyy-ww\') AS agg_time")
+    sql_group_by <- glue::glue(sql_group_by, ", agg_time")
+  }
 
   # Additions to where partial
   # Sexo
@@ -89,7 +103,7 @@ get_sih <- function(agg, ano, pcdas_token = NULL, sexo = NULL, idade_a = NULL, i
   sql_query <- glue::glue(sql_select, sql_from, sql_where, sql_group_by, .sep = " ")
 
   # Create list with token and SQL query
-  request_body <- list(token = list(token = pcdas_token), sql = list(sql = list(query = sql_query, fetch_size = 10000)))
+  request_body <- list(token = list(token = pcdas_token), sql = list(sql = list(query = sql_query, fetch_size = fetch_size)))
 
   # Request body as JSON
   request_body_json <- jsonlite::toJSON(request_body, auto_unbox = TRUE)
@@ -98,14 +112,14 @@ get_sih <- function(agg, ano, pcdas_token = NULL, sexo = NULL, idade_a = NULL, i
   content <- pcdas_query_request(body = request_body_json)
 
   # Transform content to data.frame and tibble
-  content_df <- convert_content_to_df(content) %>%
+  content_df <- convert_list_content_to_df(content) %>%
     tibble::as_tibble() %>%
     dplyr::mutate(
-      ano = ano,
       agg = as.numeric(.data$agg),
+      agg_time = .data$agg_time,
       freq = as.numeric(.data$freq)
     ) %>%
-    dplyr::select(.data$agg, .data$ano, .data$freq)
+    dplyr::select(.data$agg, .data$agg_time, .data$freq)
 
   return(content_df)
 }
